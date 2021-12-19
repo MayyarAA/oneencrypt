23a358217c96556f4e413f3ba4f75df80cb40a2b
MayyarAA <mayyar1235@gmail.com>
2021-12-19 06:51:59 -0500

    intial proj

---
+++ src/main/java/com/oneencrypt/oneencrypt/OneencryptApplication.java
@@ -0,0 +1,13 @@
+package com.oneencrypt.oneencrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneencryptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneencryptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oneencrypt/oneencrypt/OneencryptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oneencrypt.oneencrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneencryptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
