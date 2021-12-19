a254109d9871a54b1dcd65da48efc6befac904d4
MayyarAA <mayyar1235@gmail.com>
2021-12-19 09:04:45 -0500

    able to write to file with good checks

--- src/main/java/com/oneencrypt/oneencrypt/central/WriteToFileService.java
+++ src/main/java/com/oneencrypt/oneencrypt/central/WriteToFileService.java
@@ -7,20 +7,57 @@ public class WriteToFileService {
         String fileName = "temp";
         String filePath = "/Users/mayyaral-atari/Desktop/JAVAoneencrypt/";
         String filePathName =  createFileName(fileName,filePath);
-        boolean isFileCreated= createFile(filePathName);
-        if(!isFileCreated){
-            return;
-        }
-        boolean writeToFileResult = writeToFile(filePathName);
-
+        boolean fileExists = checkIfFileExists(filePathName);
+        File file = new File(filePathName);
+        if(!fileExists){createFile(filePathName);}
+        if(!checkIfFileIsWorkable(file)){System.out.println("File is not workable check permissions");return;}
+        HashMap<String,String> keyValuePair = new HashMap<>();
+        buildKeyValuePair(keyValuePair);
+        boolean writeToFileResult = writeToFile(filePathName,keyValuePair);
+        if(!writeToFileResult){System.out.println("Error File was not written to0");return;}
+        System.out.println("Success File was written too ");
     }
 
-    private static boolean writeToFile(String filePathName){
-//        FileWriter fw  = new FileWriter();
+    private static boolean writeToFile(String filePathName, HashMap<String,String> keyValuePair){
+        if(!writeToFileWithKeyValueMap(filePathName,keyValuePair)) return false;
+        return true;
+    }
+    private static boolean writeToFileWithKeyValueMap(String filePathName,HashMap<String,String> keyValuePair ){
+        try{
+            FileWriter fileWriter = new FileWriter(filePathName);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            for(String key:keyValuePair.keySet()){
+                String keyValueStr = key + " : " + keyValuePair.getOrDefault(key," null");
+                bufferedWriter.write(keyValueStr);
+                bufferedWriter.newLine();
+
+            }
+            bufferedWriter.close();
+            fileWriter.close();
+        }catch(IOException e){
+            System.out.println("Write to File error from writeToFileWithKeyValueMap");
+            e.printStackTrace();
+        }
         return true;
     }
 
+    private static void buildKeyValuePair(HashMap<String,String> keyValuePair){
+        keyValuePair.put("k1","p1");
+        keyValuePair.put("k2","p2");
+    }
 
+    private static boolean checkIfFileIsWorkable(File file){
+        if(!file.canWrite()) return false;
+        if(!file.canRead()) return false;
+        return true;
+    }
+    private static boolean checkIfFileExists(String filePathName){
+        File file = new File(filePathName);
+        if(file.exists()){
+            return true;
+        }
+        return false;
+    }
 
     private static boolean createFile(String filePathName){
         try{
