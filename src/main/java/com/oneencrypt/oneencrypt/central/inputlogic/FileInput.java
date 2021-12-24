bd056aedc861a17243b9f6895bf1ce449188f953
MayyarAA <mayyar1235@gmail.com>
2021-12-23 23:39:23 -0500

    fixed error from reading file

--- src/main/java/com/oneencrypt/oneencrypt/central/inputlogic/FileInput.java
+++ src/main/java/com/oneencrypt/oneencrypt/central/inputlogic/FileInput.java
@@ -7,6 +7,8 @@ import com.oneencrypt.oneencrypt.central.dataobjects.KeyValueObject;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class FileInput implements Input{
@@ -31,6 +33,7 @@ public class FileInput implements Input{
             readFileService();
         }catch(Exception e){
             System.out.println("Error: Set File for input first FileInput>run");
+            e.printStackTrace();
             return;
         }
 
@@ -43,15 +46,26 @@ public class FileInput implements Input{
 
     public void readFileService() throws FileNotFoundException {
         Scanner scanner = new Scanner(file);
-        while(scanner.hasNextLine()){
-            KeyValueObject keyValue = new KeyValueObject();
-            keyValue.setKey(scanner.next());
-            scanner.next();
-            keyValue.setValue(scanner.next());
-            addToDataStore(keyValue);
-        }
-        scanner.close();
 
+
+//            while(scanner.hasNextLine() ){
+            while(scanner.hasNext() ){
+                KeyValueObject keyValue = new KeyValueObject();
+                String key = "null";
+                String pwd = "null";
+                try{
+                    if(scanner.hasNext()){key = (scanner.next());;}
+                    keyValue.setKey(key);
+                    scanner.next();
+                    if(scanner.hasNext()){pwd = (scanner.next());}
+                    keyValue.setValue(pwd);
+                    addToDataStore(keyValue);
+                }catch(NoSuchElementException e){
+                    System.out.println(" readFileService Scanner read empty line");
+                    e.printStackTrace();
+                }
+            }
+            scanner.close();
     }
 
     public void addToDataStore(KeyValueObject keyValueObject) {
