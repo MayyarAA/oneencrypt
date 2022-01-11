1447bfc3ffd4d85b3298d9af15e0705248b82b89
MayyarAA <mayyar1235@gmail.com>
2022-01-10 23:15:04 -0500

    able to download file

--- src/main/java/com/oneencrypt/oneencrypt/central/api/input/InputAPI.java
+++ src/main/java/com/oneencrypt/oneencrypt/central/api/input/InputAPI.java
@@ -1,13 +1,22 @@
 package com.oneencrypt.oneencrypt.central.api.input;
 
 import com.oneencrypt.oneencrypt.central.dataobjects.KeyValueObject;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 @RestController
@@ -16,25 +25,31 @@ public class InputAPI {
 
     //add values w/ existing file
     @PostMapping(path = "addValues", consumes = {MediaType.APPLICATION_JSON_VALUE})
-    public String addValues(@RequestBody ArrayList<KeyValueObject> keyValueListObj) {
-        if (keyValueListObj == null) return "Null KeyValueList";
+    public ResponseEntity<Resource> addValues(@RequestBody ArrayList<KeyValueObject> keyValueListObj) throws IOException {
+        if (keyValueListObj == null) return null;
         InputAPIServices inputAPIServices = new InputAPIServices();
         inputAPIServices.createFileAddValuesEncrypted(keyValueListObj);
         File file = inputAPIServices.returnFile();
         //how to return file
-        return "String from addValues" + keyValueListObj.get(0).getValue();
-    }
+        HttpHeaders header = new HttpHeaders();
+        header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getName());
+        header.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        header.add("Pragma", "no-cache");
+        header.add("Expires", "0");
+        try{
 
-//    //add values w/ existing file
-//    @PostMapping(path = "addValues", consumes = {MediaType.APPLICATION_JSON_VALUE})
-//    public String addValues(@RequestBody ArrayList<KeyValueObject> keyValueListObj) {
-//        if (keyValueListObj == null) return "Null KeyValueList";
-//        InputAPIServices inputAPIServices = new InputAPIServices();
-//        inputAPIServices.createFileAddValuesEncrypted(keyValueListObj);
-//        File file = inputAPIServices.returnFile();
-//        //how to return file
-//        return "String from addValues" + keyValueListObj.get(0).getValue();
-//    }
+        Path path = Paths.get(file.getAbsolutePath());
+        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+        return ResponseEntity.ok()
+                .headers(header)
+                .contentLength(file.length())
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(resource);
+        }catch (IOException ioException){
+            ioException.printStackTrace();
+            return new ResponseEntity<Resource>(HttpStatus.BAD_REQUEST);
+        }
+    }
 
     @PostMapping(path = "addValue", consumes = {MediaType.APPLICATION_JSON_VALUE})
     public String addValue(@RequestBody KeyValueObject keyValueObject) {
